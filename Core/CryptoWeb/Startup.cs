2f347020c1fdb7c206a07cf8981116a1ef6ac76a
Hakim Ghods <ghods.hakim@hotmail.com>
2018-12-27 02:33:21 +0330

    Clean-up

---
+++ Core/CryptoWeb/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
+
+namespace CryptoWeb
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+             byte[] salt = Encoding.ASCII.GetBytes("salt");
+
+            const int numberOfRequests = 10;
+
+            
+
+            app.Run(async (context) =>
+            {
+                for (int i = 1; i < numberOfRequests+1; i++)
+            {
+                var start = DateTime.Now;
+                KeyDerivation.Pbkdf2("password", salt, KeyDerivationPrf.HMACSHA512, 10000, 512);
+                var duration = (DateTime.Now - start);
+                //Console.WriteLine($"{i} => {duration.TotalMilliseconds}");
+                await context.Response.WriteAsync($"{i} => {duration.TotalMilliseconds}");
+            }
+                
+            });
+        }
+    }
+}
