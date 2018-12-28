16e4606d280accce9ee632a338fb4ff6ae32516c
Hakim Ghods <ghods.hakim@hotmail.com>
2018-12-28 16:39:43 +0330

    Cleanup

--- Core/NetCryptoApp/Program.cs
+++ Core/NetCryptoApp/Program.cs
@@ -7,18 +7,20 @@ namespace NetCryptoApp
 {
     class Program
     {
+        const int numberOfRequests = 100;
+        const int iterationCount = 10000;
+
         static async Task Main(string[] args)
         {
             byte[] salt = Encoding.ASCII.GetBytes("salt");
 
-            const int numberOfRequests = 10;
             var baseTime = DateTime.Now;
 
             if (args != null && args[0] == "async")
             {
                 await TestAsync(salt, numberOfRequests, baseTime);
             }
-            else if(args[0] == "sync")
+            else if (args[0] == "sync")
             {
                 TestSync(salt, numberOfRequests, baseTime);
             }
@@ -40,16 +42,9 @@ namespace NetCryptoApp
                 {
                     CryptoPbkdf2(salt, out int cthreadId);
                     threadId = cthreadId;
-
                 });
-
                 var duration = (DateTime.Now - start);
-
-                //ThreadPool.GetAvailableThreads(out int workerThreads, out int completionPortThreads);
-                //Console.WriteLine($" Worker threads : {workerThreads}");
-
                 Console.WriteLine($"{i} => [{threadId.ToString()}] == {Math.Ceiling((start - baseTime).TotalMilliseconds)} - {Math.Ceiling(duration.TotalMilliseconds)}");
-
             }
         }
 
@@ -61,7 +56,7 @@ namespace NetCryptoApp
             for (int i = 1; i < numberOfRequests + 1; i++)
             {
                 var start = DateTime.Now;
-                KeyDerivation.Pbkdf2("password", salt, KeyDerivationPrf.HMACSHA512, 10000, 512);
+                KeyDerivation.Pbkdf2("password", salt, KeyDerivationPrf.HMACSHA512, iterationCount, 512);
                 var duration = (DateTime.Now - start);
 
                 Console.WriteLine($"{i} => [{Thread.CurrentThread.ManagedThreadId}] == {Math.Ceiling((start - baseTime).TotalMilliseconds)} - {Math.Ceiling(duration.TotalMilliseconds)}");
@@ -73,11 +68,11 @@ namespace NetCryptoApp
             ThreadPool.GetAvailableThreads(out int workerThreads, out int completionPortThreads);
             Console.WriteLine($" Worker threads : {workerThreads}");
 
-            Parallel.For(0, numberOfRequests, index => {
+            Parallel.For(0, numberOfRequests, index =>
+            {
                 var start = DateTime.Now;
-                KeyDerivation.Pbkdf2("password", salt, KeyDerivationPrf.HMACSHA512, 10000, 512);
+                KeyDerivation.Pbkdf2("password", salt, KeyDerivationPrf.HMACSHA512, iterationCount, 512);
                 var duration = (DateTime.Now - start);
-
                 Console.WriteLine($"{index} => [{Thread.CurrentThread.ManagedThreadId}] == {Math.Ceiling((start - baseTime).TotalMilliseconds)} - {Math.Ceiling(duration.TotalMilliseconds)}");
             });
         }
@@ -85,9 +80,7 @@ namespace NetCryptoApp
         public static void CryptoPbkdf2(byte[] salt, out int threadId)
         {
             threadId = Thread.CurrentThread.ManagedThreadId;
-            KeyDerivation.Pbkdf2("password", salt, KeyDerivationPrf.HMACSHA512, 10000, 512).ToString();
-
-
+            KeyDerivation.Pbkdf2("password", salt, KeyDerivationPrf.HMACSHA512, iterationCount, 512).ToString();
         }
     }
 }
